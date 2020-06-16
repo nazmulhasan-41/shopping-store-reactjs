3b60037845d8324874612f1bc3a3b97f8e5558d1
unknown <najmulhasan041@gmail.com>
2020-06-16 15:16:12 +0600

    first caaaaaaaaaaaaaaaaaogggmmit

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -31,10 +31,10 @@ export default class MyApp extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
-            //sandbox:    process.env.REACT_APP_APP_ID,
+            sandbox:    process.env.REACT_APP_APP_ID,
 
 
-            sandbox: "ASiwMEZz84C9RCilnnltSJ1FmRs8dO2fLgXQjTQQuDdtefXzW3gK_K6MRTNzKJ4HL82QQyn29GCs5FSh",
+            //sandbox: "ASiwMEZz84C9RCilnnltSJ1FmRs8dO2fLgXQjTQQuDdtefXzW3gK_K6MRTNzKJ4HL82QQyn29GCs5FSh",
 
             production: 'YOUR-PRODUCTION-APP-ID',
         }
